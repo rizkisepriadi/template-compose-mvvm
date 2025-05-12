107f34e0aa22b30bbb9da8a63af25914a98bf9b9
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-12 16:54:32 +0700

    feat: update MainActivity.kt

--- app/src/main/java/com/app/template_compose_mvvm/ui/features/main/MainActivity.kt
+++ app/src/main/java/com/app/template_compose_mvvm/ui/features/main/MainActivity.kt
@@ -3,13 +3,26 @@ package com.app.template_compose_mvvm.ui.features.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.app.template_compose_mvvm.ui.AppContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AppContent()
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppContent()
+                }
+            }
         }
     }
 }
